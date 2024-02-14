74f7eb4407392e1eca756cc30f3d4c8113037ee4
Hualong Qiao <h3qiao@uwaterloo.ca>
2024-02-14 11:40:33 -0500

    BE-15: add back DeserializationErrorMapper

--- src/main/java/infrastructure/dto/serialization/DeserializationErrorMapper.java
+++ src/main/java/infrastructure/dto/serialization/DeserializationErrorMapper.java
@@ -1,27 +1,27 @@
-//package infrastructure.dto.serialization;
-//import com.fasterxml.jackson.databind.JsonMappingException;
-//
-//import infrastructure.dto.ApiResponse;
-//import infrastructure.dto.out.listing.ListingDetailsDto;
-//import jakarta.ws.rs.core.MediaType;
-//import jakarta.ws.rs.core.Response;
-//import jakarta.ws.rs.ext.ExceptionMapper;
-//import jakarta.ws.rs.ext.Provider;
-//
-//import java.util.Optional;
-//
-//@Provider
-//public class DeserializationErrorMapper implements ExceptionMapper<JsonMappingException> {
-//    @Override
-//    public Response toResponse(JsonMappingException e) {
-//        String message = "Error processing JSON data. Please check the request payload.";
-//        ApiResponse<ListingDetailsDto> response = new ApiResponse<>(Optional.of(message),
-//                400,
-//                Optional.empty());
-//
-//        return Response.status(Response.Status.BAD_REQUEST)
-//                .entity(response)
-//                .type(MediaType.APPLICATION_JSON)
-//                .build();
-//    }
-//}
+package infrastructure.dto.serialization;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+import infrastructure.dto.ApiResponse;
+import infrastructure.dto.out.listing.ListingDetailsDto;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+import java.util.Optional;
+
+@Provider
+public class DeserializationErrorMapper implements ExceptionMapper<JsonMappingException> {
+    @Override
+    public Response toResponse(JsonMappingException e) {
+        String message = "Error processing JSON data. Please check the request payload.";
+        ApiResponse<ListingDetailsDto> response = new ApiResponse<>(Optional.of(message),
+                400,
+                Optional.empty());
+
+        return Response.status(Response.Status.BAD_REQUEST)
+                .entity(response)
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
+}
