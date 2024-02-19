316534c6b1d559f5590984b3bc87c419662c26f2
Hualong Qiao <h3qiao@uwaterloo.ca>
2024-02-19 16:41:34 -0500

    BE-39: Add CI/CD for main branch

---
+++ deploy_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+pkill -f quarkus-run.jar
+sleep 5
+
+# shellcheck disable=SC2164
+cd /home/ubuntu/backend
+
+./mvnw package
+
+nohup java -jar target/quarkus-app/quarkus-run.jar > app.log 2>&1 &
+
+echo "Deployment completed."
