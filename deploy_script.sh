60c5d9befcdfccd10200b8e62490c60784fbbca9
Hualong Qiao <h3qiao@uwaterloo.ca>
2024-02-19 22:23:46 -0500

    main: export environment variable through shell script

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
+source ~/env.sh
+
 export MAVEN_HOME=/opt/apache-maven-3.9.6
 export PATH=$PATH:$MAVEN_HOME/bin
 
 export JAVA_HOME=/opt/amazon-corretto-17.0.10.8.1-linux-x64
 export PATH=$PATH:$JAVA_HOME/bin
-
-export DB_USERNAME=$DB_USERNAME
-export DB_PASSWORD=$DB_PASSWORD
-export DB_NAME=$DB_NAME
-
-echo "DB username: $DB_USERNAME"
-echo "DB password: $DB_PASSWORD"
-echo "DB name: $DB_NAME"
 
 pkill -f quarkus-run.jar
 sleep 5
@@ -22,5 +16,7 @@ cd /home/ubuntu/backend
 ./mvnw package
 
 nohup java -jar target/quarkus-app/quarkus-run.jar > app.log 2>&1 &
+
+rm ~/env.sh
 
 echo "Deployment completed."
