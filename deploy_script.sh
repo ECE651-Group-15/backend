6754a398008f82d4468a3ce26bd362ad15937c5f
Hualong Qiao <h3qiao@uwaterloo.ca>
2024-02-19 21:24:33 -0500

    main: manually specify java and mvn location

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-source ~/.bashrc
+export MAVEN_HOME=/opt/apache-maven-3.9.6
+export PATH=$PATH:$MAVEN_HOME/bin
+
+export JAVA_HOME=/opt/amazon-corretto-17.0.10.8.1-linux-x64
+export PATH=$PATH:$JAVA_HOME/bin
 
 pkill -f quarkus-run.jar
 sleep 5
